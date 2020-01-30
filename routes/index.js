5e80f0f04c39fa39517e5d3769d5b11946f88b37
Muna <mzam66@gmail.com>
2020-01-30 18:57:10 +0000

    json data parsed in index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var products = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+  {
+    title: "Food Market",
+    products: products
+  });
 });
 
 module.exports = router;
