b484a6f0981430a93f3c85ae81aa0658940c1fdc
Muna <mzam66@gmail.com>
2020-01-30 21:40:30 +0000

    add function added for basket

--- src/basket.js
+++ src/basket.js
@@ -6,4 +6,17 @@ module.exports = function Basket(inBasket) {
   this.discountAmt = inBasket.discountAmt;
   this.total = inBasket.total;
   this.currency = "USD"
+
+  this.add = function(item, id) {
+    var basketItem = this.items[id];
+    if (!basketItem) {
+      basketItem = this.items[id] = { item: item,
+                                      quantity: 0,
+                                      price: 0 };
+    };
+    basketItem.quantity++;
+    basketItem.price = basketItem.item.price * basketItem.quantity;
+    this.totalItems++;
+    this.total += basketItem.price;
+  };
 };
