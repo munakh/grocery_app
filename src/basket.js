66a829498447e1548bcd14aab6a15e50ca1dec24
Muna <mzam66@gmail.com>
2020-01-30 20:49:03 +0000

    basket properties added

--- src/basket.js
+++ src/basket.js
@@ -1,3 +1,9 @@
 module.exports = function Basket(items) {
-  
+  this.items = items.items;
+  this.totalItems = items.totalItems;
+  this.subtotal = items.subtotal;
+  this.discounts = [];
+  this.discountAmt = items.discountAmt;
+  this.total = items.total;
+  this.currency = "USD"
 };
